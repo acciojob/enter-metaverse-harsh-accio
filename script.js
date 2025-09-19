52c76c983cee7ade5d68fac7decd119cb2a5f78e
harsh-accio <rajputharsh9897@gmail.com>
2025-09-19 12:11:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+function changeToHeading() {
+	const para =document.getElementById('status') 
+	
+	const heading = document.createElement('h1')
+	heading.innerText="Entered Metaverse";
+	
+	para.parentNode.replaceChild(heading,para);
+}
