49f7a003cf3ec258fed609f6dfd6e17006d6a321
harsh-accio <rajputharsh9897@gmail.com>
2025-09-19 12:24:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 //your JS code here. If required.
 function changeToHeading() {
-	const para =document.getElementById('status') 
-	
-	const heading = document.createElement('h1')
-	heading.innerText="Entered Metaverse";
-	heading.id = "status";
-	
-	para.parentNode.replaceChild(heading,para);
-}
+    // get the paragraph
+    const para = document.getElementById('status');
+
+    // create a new h1 element
+    const heading = document.createElement('h1');
+    heading.innerText = "Entered Metaverse";
+
+    // keep the same id so Cypress can find it
+    heading.id = "status";
+
+    // replace p with h1
+    para.parentNode.replaceChild(heading, para);
+  }
